d2d87b18ff8bd144cec9cf0cd0e4c32401edf6e6
Roman Bulinski <aboandrus@gmail.com>
2019-08-02 14:31:04 +0200

    Introduced BigInteger variables insteadof int

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,43 +8,43 @@ public class Main {
 
     public static BigInteger diagonal(int n, int p) {
 
-        int one = 1;
-
         int counter = 2;
-        List<int[]> piramid = new ArrayList<>();
-        piramid.add(new int[]{1});
-        piramid.add(new int[]{1, 1});
+        BigInteger one = new BigInteger("1");
+
+        List< BigInteger[]> piramid = new ArrayList<>();
 
         if (n == 0) {
-            piramid.add(new int[]{1});
+            piramid.add(new BigInteger[]{one});
         } else if (n == 1) {
-            piramid.add(new int[]{1, 1});
+            piramid.add(new BigInteger[]{one, one});
         } else if (n > 1) {
+            piramid.add(new BigInteger[]{one});
+            piramid.add(new BigInteger[]{one, one});
+
             while (counter <= n) {
+                BigInteger[] prev = piramid.get(counter-1);
+                BigInteger[] temp = new BigInteger[counter+1];
 
-                int[] prev = piramid.get(counter-1);
-                int[] temp = new int[counter+1];
-
-                temp[0]=1;
+                temp[0]=one;
                 for( int i=1; i<temp.length-1; i++){
-                    temp[i]=prev[i-1]+prev[i];
+                    temp[i]= prev[i-1].add(prev[i]);
                 }
-                temp[counter]=1;
+                temp[counter]=one;
 
                 piramid.add(temp);
                 counter++;
             }
         }
 
-        piramid.forEach(j-> Collections.singletonList(j).forEach(m-> System.out.println(Arrays.toString(m))) );
+//        piramid.forEach(j-> Collections.singletonList(j).forEach(m-> System.out.println(Arrays.toString(m))) );
 
-        int sum =0;
+        BigInteger sum = new BigInteger(String.valueOf(0));
         for(int j = p; j<=n; j++){
-            sum = sum +piramid.get(j)[p];
+            sum=sum.add( new BigInteger(  String.valueOf( piramid.get(j)[p]) ));
         }
 
         System.out.println(sum);
-        return new BigInteger(String.valueOf(sum));
+        return sum;
     }
 
 
